458b5e6a70831fe668e33a67da72418fbee7ddc3
Henri Barboza <henr.fe7@gmail.com>
2024-07-02 19:08:04 -0300

    Criaçãcao do sistema de login e register com breeze

---
+++ HLVendas/resources/views/Login/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Entre</h1>
+    <form action="">
+        @CSRF
+        <div>
+            <label for="">Login</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Senha</label>
+            <input type="password">
+        </div>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
